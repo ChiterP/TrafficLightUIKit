14929537e04f35526872f118f745a4d935dcaa5b
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-06-01 10:48:17 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by 18316409 on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
