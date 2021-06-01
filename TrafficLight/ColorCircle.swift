764fc014bb981ee21b9cfc8831e09061953e59d8
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-06-01 23:54:45 +0300

    done

---
+++ TrafficLight/ColorCircle.swift
@@ -0,0 +1,25 @@
+//
+//  ColorCircle.swift
+//  TrafficLight
+//
+//  Created by 18316409 on 01.06.2021.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    let color: Color
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .frame(width: 100, height: 100)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle(color: .yellow)
+    }
+}
